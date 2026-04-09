da350ddb25b56336a3550f125f5c64d55df67d48
joppetk <168833982+joppetk@users.noreply.github.com>
2026-04-09 08:33:09 +0400

    Update setup.sh

--- companion_computer/setup.sh
+++ companion_computer/setup.sh
@@ -4,4 +4,4 @@ set -e
 python3 -m venv --system-site-packages drone-venv
 source drone-venv/bin/activate
 python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python -m pip install -r companion_computer/requirements.txt
